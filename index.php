010efb07818727a7f63a7ea223223983583c698f
Robert Lawson <rolawson@me.com>
2013-04-24 14:18:43 -0700

    functionalty

--- index.php
+++ index.php
@@ -27,12 +27,12 @@
         
         //Email
         
-        var email = $("#email_field").val();
         $("#email_field").onmousedown(function() {
-            if (email == "Email Address") $("#email_field").val("");
+            if (this.val() == "Email Address") this.val("");
         });
-        $("#email_submit").click(function() {
         
+        $("#email_submit").click(function() {
+            var email = $("#email_field").val();
         	if ((email.length != 0) && (email.indexOf("@") !== -1)) {
             $.get('service/email.php', {email: email}, "json");
             $("#response").text("The email address "+email+" has been submitted.");
