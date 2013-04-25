e05d7f4088db0b1af8161dd0a396fba4de55768e
Robert Lawson <rolawson@me.com>
2013-04-25 16:55:58 -0700

    php error reporting

--- service/email.php
+++ service/email.php
@@ -1,6 +1,7 @@
 <?php
-# This function reads your DATABASE_URL configuration automatically set by Heroku
-# the return value is a string that will work with pg_connect
+ini_set('display_errors',1); 
+ error_reporting(E_ALL);
+
 function pg_connection_string() {
   return "dbname=d321aem4kva19e host=ec2-54-225-112-205.compute-1.amazonaws.com port=5432 user=egauidjrujdzuf password=4fzndpfmO_mv_rGp1RHqj8j6ub sslmode=require";
 }
