6bd791de4fea12940ba6148ae315daa419ab0049
Robert Lawson <rolawson@me.com>
2013-04-24 21:59:56 -0700

    email php

--- service/email.php
+++ service/email.php
@@ -1,15 +1,17 @@
-<?php 
-error_reporting(-1);
-session_start();
-$username="strobefl_tlabs";
-$password="st0lenbarr0w";
-$database="strobefl_labs";
-
-mysql_connect('localhost',$username,$password);
-@mysql_select_db($database) or die( "Unable to select database");
+<?php
+# This function reads your DATABASE_URL configuration automatically set by Heroku
+# the return value is a string that will work with pg_connect
+function pg_connection_string() {
+  return "dbname=d321aem4kva19e host=ec2-54-225-112-205.compute-1.amazonaws.com port=5432 user=egauidjrujdzuf password=4fzndpfmO_mv_rGp1RHqj8j6ub sslmode=require";
+}
+ 
+# Establish db connection
+$db = pg_connect(pg_connection_string());
+if (!$db) {
+   echo "Database connection error."
+   exit;
+}
 
 $email = $_GET["email"];
-$email = mysql_real_escape_string($email);
-
-mysql_query("INSERT INTO `goboldly-emails` (email) VALUES ('$email')")
+$result = pg_query($db, "INSERT INTO emails (address) VALUES ('$email')" );
 ?>
